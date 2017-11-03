4bfe6b23288a3b3586c0e8c1ab588ddfc3a9d308
Francisco Molina <francisco.molina@outlook.cl>
2017-11-03 00:11:35 -0300

    cleaup & formated code

--- Google.Authenticator/TwoFactorAuthenticator.cs
+++ Google.Authenticator/TwoFactorAuthenticator.cs
@@ -8,13 +8,20 @@ namespace Google.Authenticator
 {
     public class TwoFactorAuthenticator
     {
-        public static DateTime _epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        public TimeSpan DefaultClockDriftTolerance { get; set; }
-        public bool UseManagedSha1Algorithm { get; set; }
-        public bool TryUnmanagedAlgorithmOnFailure { get; set; }
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
 
-        public TwoFactorAuthenticator() : this(true, true) {}
+        // ReSharper disable once MemberCanBePrivate.Global
+        public TimeSpan DefaultClockDriftTolerance { get; }
+        // ReSharper disable once MemberCanBePrivate.Global
+        public bool UseManagedSha1Algorithm { get; }
+        // ReSharper disable once MemberCanBePrivate.Global
+        public bool TryUnmanagedAlgorithmOnFailure { get; }
 
+        public TwoFactorAuthenticator() : this(true, true)
+        {
+        }
+
+        // ReSharper disable once MemberCanBePrivate.Global
         public TwoFactorAuthenticator(bool useManagedSha1, bool useUnmanagedOnFail)
         {
             DefaultClockDriftTolerance = TimeSpan.FromMinutes(5);
@@ -23,7 +30,7 @@ namespace Google.Authenticator
         }
 
         /// <summary>
-        /// Generate a setup code for a Google Authenticator user to scan.
+        ///     Generate a setup code for a Google Authenticator user to scan.
         /// </summary>
         /// <param name="accountTitleNoSpaces">Account Title (no spaces)</param>
         /// <param name="accountSecretKey">Account Secret Key</param>
@@ -36,21 +43,7 @@ namespace Google.Authenticator
         }
 
         /// <summary>
-        /// Generate a setup code for a Google Authenticator user to scan (with issuer ID).
-        /// </summary>
-        /// <param name="issuer">Issuer ID (the name of the system, i.e. 'MyApp')</param>
-        /// <param name="accountTitleNoSpaces">Account Title (no spaces)</param>
-        /// <param name="accountSecretKey">Account Secret Key</param>
-        /// <param name="qrCodeWidth">QR Code Width</param>
-        /// <param name="qrCodeHeight">QR Code Height</param>
-        /// <returns>SetupCode object</returns>
-        public SetupCode GenerateSetupCode(string issuer, string accountTitleNoSpaces, string accountSecretKey, int qrCodeWidth, int qrCodeHeight)
-        {
-            return GenerateSetupCode(issuer, accountTitleNoSpaces, accountSecretKey, qrCodeWidth, qrCodeHeight, false);
-        }
-
-        /// <summary>
-        /// Generate a setup code for a Google Authenticator user to scan (with issuer ID).
+        ///     Generate a setup code for a Google Authenticator user to scan (with issuer ID).
         /// </summary>
         /// <param name="issuer">Issuer ID (the name of the system, i.e. 'MyApp')</param>
         /// <param name="accountTitleNoSpaces">Account Title (no spaces)</param>
@@ -59,30 +52,29 @@ namespace Google.Authenticator
         /// <param name="qrCodeHeight">QR Code Height</param>
         /// <param name="useHttps">Use HTTPS instead of HTTP</param>
         /// <returns>SetupCode object</returns>
-        public SetupCode GenerateSetupCode(string issuer, string accountTitleNoSpaces, string accountSecretKey, int qrCodeWidth, int qrCodeHeight, bool useHttps)
+        // ReSharper disable once MemberCanBePrivate.Global
+        public SetupCode GenerateSetupCode(string issuer, string accountTitleNoSpaces, string accountSecretKey, int qrCodeWidth, int qrCodeHeight, bool useHttps = false)
         {
-            if (accountTitleNoSpaces == null) { throw new NullReferenceException("Account Title is null"); }
+            if (accountTitleNoSpaces == null) throw new NullReferenceException("Account Title is null");
 
             accountTitleNoSpaces = accountTitleNoSpaces.Replace(" ", "");
 
-            SetupCode sC = new SetupCode();
-            sC.Account = accountTitleNoSpaces;
-            sC.AccountSecretKey = accountSecretKey;
+            var sC = new SetupCode
+            {
+                Account = accountTitleNoSpaces,
+                AccountSecretKey = accountSecretKey
+            };
 
-            string encodedSecretKey = EncodeAccountSecretKey(accountSecretKey);
+            var encodedSecretKey = EncodeAccountSecretKey(accountSecretKey);
             sC.ManualEntryKey = encodedSecretKey;
 
-            string provisionUrl = null;
-            
-            if (string.IsNullOrEmpty(issuer))
-            {
-                provisionUrl = UrlEncode(String.Format("otpauth://totp/{0}?secret={1}", accountTitleNoSpaces, encodedSecretKey));
-            } else {
-                provisionUrl = UrlEncode(String.Format("otpauth://totp/{0}?secret={1}&issuer={2}", accountTitleNoSpaces, encodedSecretKey, UrlEncode(issuer)));
-            }
+            var provisionUrl = 
+                UrlEncode(string.IsNullOrEmpty(issuer) ? 
+                $"otpauth://totp/{accountTitleNoSpaces}?secret={encodedSecretKey}" :
+                $"otpauth://totp/{accountTitleNoSpaces}?secret={encodedSecretKey}&issuer={UrlEncode(issuer)}");
 
-            string protocol = useHttps ? "https" : "http";
-            string url = String.Format("{0}://chart.googleapis.com/chart?cht=qr&chs={1}x{2}&chl={3}", protocol, qrCodeWidth, qrCodeHeight, provisionUrl);
+            var protocol = useHttps ? "https" : "http";
+            var url = $"{protocol}://chart.googleapis.com/chart?cht=qr&chs={qrCodeWidth}x{qrCodeHeight}&chl={provisionUrl}";
 
             sC.QrCodeSetupImageUrl = url;
 
@@ -91,70 +83,52 @@ namespace Google.Authenticator
 
         private string UrlEncode(string value)
         {
-            StringBuilder result = new StringBuilder();
-            string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~";
+            var result = new StringBuilder();
+            const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.~";
 
-            foreach (char symbol in value)
-            {
+            foreach (var symbol in value)
                 if (validChars.IndexOf(symbol) != -1)
-                {
                     result.Append(symbol);
-                }
                 else
-                {
-                    result.Append('%' + String.Format("{0:X2}", (int)symbol));
-                }
-            }
+                    result.Append('%' + $"{(int) symbol:X2}");
 
             return result.ToString().Replace(" ", "%20");
         }
 
         private string EncodeAccountSecretKey(string accountSecretKey)
         {
-            //if (accountSecretKey.Length < 10)
-            //{
-            //    accountSecretKey = accountSecretKey.PadRight(10, '0');
-            //}
-
-            //if (accountSecretKey.Length > 12)
-            //{
-            //    accountSecretKey = accountSecretKey.Substring(0, 12);
-            //}
-
             return Base32Encode(Encoding.UTF8.GetBytes(accountSecretKey));
         }
 
+        // ReSharper disable once MemberCanBeMadeStatic.Local
         private string Base32Encode(byte[] data)
         {
-            int inByteSize = 8;
-            int outByteSize = 5;
-            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
+            const int inByteSize = 8;
+            const int outByteSize = 5;
+            var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567".ToCharArray();
 
-            int i = 0, index = 0, digit = 0;
-            int current_byte, next_byte;
-            StringBuilder result = new StringBuilder((data.Length + 7) * inByteSize / outByteSize);
+            int i = 0, index = 0;
+            var result = new StringBuilder((data.Length + 7) * inByteSize / outByteSize);
 
             while (i < data.Length)
             {
-                current_byte = (data[i] >= 0) ? data[i] : (data[i] + 256); // Unsign
+                var currentByte = data[i];
 
                 /* Is the current digit going to span a byte boundary? */
-                if (index > (inByteSize - outByteSize))
+                int digit;
+                if (index > inByteSize - outByteSize)
                 {
-                    if ((i + 1) < data.Length)
-                        next_byte = (data[i + 1] >= 0) ? data[i + 1] : (data[i + 1] + 256);
-                    else
-                        next_byte = 0;
+                    var nextByte = i + 1 < data.Length ? data[i + 1] : 0;
 
-                    digit = current_byte & (0xFF >> index);
+                    digit = currentByte & (0xFF >> index);
                     index = (index + outByteSize) % inByteSize;
                     digit <<= index;
-                    digit |= next_byte >> (inByteSize - index);
+                    digit |= nextByte >> (inByteSize - index);
                     i++;
                 }
                 else
                 {
-                    digit = (current_byte >> (inByteSize - (index + outByteSize))) & 0x1F;
+                    digit = (currentByte >> (inByteSize - (index + outByteSize))) & 0x1F;
                     index = (index + outByteSize) % inByteSize;
                     if (index == 0)
                         i++;
@@ -165,61 +139,60 @@ namespace Google.Authenticator
             return result.ToString();
         }
 
+        // ReSharper disable once InconsistentNaming
         public string GeneratePINAtInterval(string accountSecretKey, long counter, int digits = 6)
         {
             return GenerateHashedCode(accountSecretKey, counter, digits);
         }
 
-        internal string GenerateHashedCode(string secret, long iterationNumber, int digits = 6)
+        private string GenerateHashedCode(string secret, long iterationNumber, int digits = 6)
         {
-            byte[] key = Encoding.UTF8.GetBytes(secret);
+            var key = Encoding.UTF8.GetBytes(secret);
             return GenerateHashedCode(key, iterationNumber, digits);
         }
 
-        internal string GenerateHashedCode(byte[] key, long iterationNumber, int digits = 6)
+        private string GenerateHashedCode(byte[] key, long iterationNumber, int digits = 6)
         {
-            byte[] counter = BitConverter.GetBytes(iterationNumber);
+            var counter = BitConverter.GetBytes(iterationNumber);
 
             if (BitConverter.IsLittleEndian)
-            {
                 Array.Reverse(counter);
-            }
 
-            HMACSHA1 hmac = getHMACSha1Algorithm(key);
-                        
-            byte[] hash = hmac.ComputeHash(counter);
+            var hmac = GetHmacSha1Algorithm(key);
 
-            int offset = hash[hash.Length - 1] & 0xf;
+            var hash = hmac.ComputeHash(counter);
+
+            var offset = hash[hash.Length - 1] & 0xf;
 
             // Convert the 4 bytes into an integer, ignoring the sign.
-            int binary =
+            var binary =
                 ((hash[offset] & 0x7f) << 24)
                 | (hash[offset + 1] << 16)
                 | (hash[offset + 2] << 8)
-                | (hash[offset + 3]);
+                | hash[offset + 3];
 
-            int password = binary % (int)Math.Pow(10, digits);
+            var password = binary % (int) Math.Pow(10, digits);
             return password.ToString(new string('0', digits));
         }
 
         private long GetCurrentCounter()
         {
-            return GetCurrentCounter(DateTime.UtcNow, _epoch, 30);
+            return GetCurrentCounter(DateTime.UtcNow, Epoch, 30);
         }
 
         private long GetCurrentCounter(DateTime now, DateTime epoch, int timeStep)
         {
-            return (long)(now - epoch).TotalSeconds / timeStep;
+            return (long) (now - epoch).TotalSeconds / timeStep;
         }
 
         /// <summary>
-        /// Creates a HMACSHA1 algorithm to use to hash the counter bytes. By default, this will attempt to use
-        /// the managed SHA1 class (SHA1Manager) and on exception (FIPS-compliant machine policy, etc) will attempt
-        /// to use the unmanaged SHA1 class (SHA1CryptoServiceProvider).
+        ///     Creates a HMACSHA1 algorithm to use to hash the counter bytes. By default, this will attempt to use
+        ///     the managed SHA1 class (SHA1Manager) and on exception (FIPS-compliant machine policy, etc) will attempt
+        ///     to use the unmanaged SHA1 class (SHA1CryptoServiceProvider).
         /// </summary>
         /// <param name="key">User's secret key, in bytes</param>
-        /// <returns>HMACSHA1 cryptographic algorithm</returns>        
-        private HMACSHA1 getHMACSha1Algorithm(byte[] key)
+        /// <returns>HMACSHA1 cryptographic algorithm</returns>
+        private HMACSHA1 GetHmacSha1Algorithm(byte[] key)
         {
             HMACSHA1 hmac;
 
@@ -227,72 +200,67 @@ namespace Google.Authenticator
             {
                 hmac = new HMACSHA1(key, UseManagedSha1Algorithm);
             }
-            catch (InvalidOperationException ioe)
+            catch (InvalidOperationException)
             {
                 if (UseManagedSha1Algorithm && TryUnmanagedAlgorithmOnFailure)
-                {
-                    try
-                    {
-                        hmac = new HMACSHA1(key, false);
-                    }
-                    catch (InvalidOperationException ioe2)
-                    {
-                        throw ioe2;
-                    }
-                }
+                    hmac = new HMACSHA1(key, false);
                 else
-                {
-                    throw ioe;
-                }
+                    throw;
             }
 
             return hmac;
         }
-        
+
+        // ReSharper disable once InconsistentNaming
         public bool ValidateTwoFactorPIN(string accountSecretKey, string twoFactorCodeFromClient)
         {
             return ValidateTwoFactorPIN(accountSecretKey, twoFactorCodeFromClient, DefaultClockDriftTolerance);
         }
 
-        public bool ValidateTwoFactorPIN(string accountSecretKey, string twoFactorCodeFromClient, TimeSpan timeTolerance)
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once MemberCanBePrivate.Global
+        public bool ValidateTwoFactorPIN(string accountSecretKey, string twoFactorCodeFromClient,TimeSpan timeTolerance)
         {
             var codes = GetCurrentPINs(accountSecretKey, timeTolerance);
             return codes.Any(c => c == twoFactorCodeFromClient);
         }
 
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once InconsistentNaming
         public string GetCurrentPIN(string accountSecretKey)
         {
             return GeneratePINAtInterval(accountSecretKey, GetCurrentCounter());
         }
-        
-         public string GetCurrentPIN(string accountSecretKey,DateTime now)
+
+        // ReSharper disable once UnusedMember.Global
+        // ReSharper disable once InconsistentNaming
+        public string GetCurrentPIN(string accountSecretKey, DateTime now)
         {
-            return GeneratePINAtInterval(accountSecretKey, GetCurrentCounter(now,_epoch,30));
+            return GeneratePINAtInterval(accountSecretKey, GetCurrentCounter(now, Epoch, 30));
         }
 
+        // ReSharper disable once InconsistentNaming
         public string[] GetCurrentPINs(string accountSecretKey)
         {
             return GetCurrentPINs(accountSecretKey, DefaultClockDriftTolerance);
         }
 
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once MemberCanBePrivate.Global
         public string[] GetCurrentPINs(string accountSecretKey, TimeSpan timeTolerance)
         {
-            List<string> codes = new List<string>();
-            long iterationCounter = GetCurrentCounter();
-            int iterationOffset = 0;
+            var codes = new List<string>();
+            var iterationCounter = GetCurrentCounter();
+            var iterationOffset = 0;
 
             if (timeTolerance.TotalSeconds > 30)
-            {
                 iterationOffset = Convert.ToInt32(timeTolerance.TotalSeconds / 30.00);
-            }
 
-            long iterationStart = iterationCounter - iterationOffset;
-            long iterationEnd = iterationCounter + iterationOffset;
+            var iterationStart = iterationCounter - iterationOffset;
+            var iterationEnd = iterationCounter + iterationOffset;
 
-            for (long counter = iterationStart; counter <= iterationEnd; counter++)
-            {
+            for (var counter = iterationStart; counter <= iterationEnd; counter++)
                 codes.Add(GeneratePINAtInterval(accountSecretKey, counter));
-            }
 
             return codes.ToArray();
         }
